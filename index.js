a78c108a13e228e87fda242abdf685736e7d5eb8
github username <github email address>
2018-01-29 09:58:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+ function distanceFromHqInBlocks(location) {
+  if (location > 42) {
+    return location - 42;
+   } else {
+     return 42 - location;
+  }
+}
+
+function distanceFromHqInFeet(location){
+   return distanceFromHqInBlocks(location) * 264;
+ }
+
+function distanceTravelledInFeet(pickupLocation, dropoffLocation){
+  return distanceFromHqInFeet(dropoffLocation) - distanceFromHqInFeet(pickupLocation);
+}
+
+function calculatesFarePrice
+ let feet = distanceTravelledInFeet(pickupLocation, dropoffLocation);
+
+ if (feet <= 400 ) {
+   return 0;
+ } else if (feet > 400 && feet <= 2000 )  {
+   return feet * 0.02;
+ } else if (feet > 2000 && feet < 2500 ) {
+   return 25;
+ } else {
+   return 'cannot travel that far';
+ }
+}
