431ae8f75cc2ead245fb3bf9bb11c53182f06d15
github username <github email address>
2018-01-29 09:59:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-// Code your solution in this file!
 
- function distanceFromHqInBlocks(location) {
-  if (location > 42) {
-    return location - 42;
+ function distanceFromHqInBlocks (pickupLocation) {
+   if (pickupLocation > 42) {
+     return pickupLocation - 42;
    } else {
-     return 42 - location;
-  }
-}
-
-function distanceFromHqInFeet(location){
-   return distanceFromHqInBlocks(location) * 264;
+     return 42 - pickupLocation;
+   }
  }
-
-function distanceTravelledInFeet(pickupLocation, dropoffLocation){
-  return distanceFromHqInFeet(dropoffLocation) - distanceFromHqInFeet(pickupLocation);
-}
-
-function calculatesFarePrice
- let feet = distanceTravelledInFeet(pickupLocation, dropoffLocation);
-
- if (feet <= 400 ) {
-   return 0;
- } else if (feet > 400 && feet <= 2000 )  {
-   return feet * 0.02;
- } else if (feet > 2000 && feet < 2500 ) {
-   return 25;
- } else {
-   return 'cannot travel that far';
+ 
+ function distanceFromHqInFeet(pickupLocation){
+   return distanceFromHqInBlocks(pickupLocation) * 264;
  }
-}
+ 
+ function distanceTravelledInFeet(pickupLocation, dropoffLocation){
+   return distanceFromHqInFeet(dropoffLocation) - distanceFromHqInFeet(pickupLocation);
+ }
+ 
+ function calculatesFarePrice (pickupLocation, dropoffLocation) {
+   let feet = distanceTravelledInFeet(pickupLocation, dropoffLocation);
+ 
+   if (feet <= 400 ) {
+     return 0;
+   } else if (feet > 400 && feet <= 2000 )  {
+     return feet * 0.02;
+   } else if (feet > 2000 && feet < 2500 ) {
+     return 25;
+   } else {
+     return 'cannot travel that far';
+   }
+ }
